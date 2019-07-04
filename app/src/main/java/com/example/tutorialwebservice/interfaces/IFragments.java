28c78d42662f9d9b3167ca5543b1dba408b75e31
HiltonRv <x3hiltonx3@gmail.com>
2019-07-04 05:24:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tutorialwebservice/interfaces/IFragments.java
@@ -0,0 +1,6 @@
+package com.example.tutorialwebservice.interfaces;
+
+import com.example.tutorialwebservice.fragments.RegistrarUsuarioFragment;
+
+public interface IFragments extends RegistrarUsuarioFragment.OnFragmentInteractionListener {
+}
